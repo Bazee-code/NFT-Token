f5a0d6cf0b71be10bea82220a130d2d25e6a80ec
Bazee-code <eugeneobazee2@gmail.com>
2025-04-08 14:18:24 +0300

    Add & set up umi instance

--- create-collection.ts
+++ create-collection.ts
@@ -1,4 +1,6 @@
-import { createNft } from "@metaplex-foundation/mpl-token-metadata";
+import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { keypairIdentity } from "@metaplex-foundation/umi";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { airdropIfRequired, getKeypairFromFile } from "@solana-developers/helpers";
 import {clusterApiUrl, Connection, LAMPORTS_PER_SOL} from '@solana/web3.js'
 
@@ -9,4 +11,14 @@ const user = await getKeypairFromFile();
 //in case wallet balance is empty
 await airdropIfRequired(connection, user.publicKey, 1 * LAMPORTS_PER_SOL, 0.5 * LAMPORTS_PER_SOL);
 
-// create umi instance
+console.log('Loaded user:', user.publicKey.toBase58());
+
+// create umi instance to interact with metaplex tools
+const umi = createUmi(connection.rpcEndpoint);
+umi.use(mplTokenMetadata());
+
+// create umi keypair
+const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+umi.use(keypairIdentity(umiUser))
+
+console.log('umi instance running');
